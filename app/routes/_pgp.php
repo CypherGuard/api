cd78813b3dbf8e4d3df9527678e7621f29dd1810
lguerra <lylian.gr@protonmail.com>
2024-07-18 10:22:06 +0200

    wip

--- app/routes/_pgp.php
+++ app/routes/_pgp.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Middleware\AuthMiddleware;
-
-$middleware = new AuthMiddleware();
-
-app()->group('/pgp', ['middleware' => [$middleware, 'handle'], function () {
+app()->group('/pgp', function () {
     app()->match('GET', '/key', "PgpController@get_public_key");
-}]);
+});
